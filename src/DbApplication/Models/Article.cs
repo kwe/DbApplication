0e95307b64cde473a859f687dfc44a471d475c50
kwe <kwevans@gmail.com>
2016-07-10 23:29:55 +0100

    adding postgresql

--- src/DbApplication/Models/Article.cs
+++ src/DbApplication/Models/Article.cs
@@ -7,6 +7,8 @@ namespace DbApplication.Models
 {
     public class Article
     {
-		
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Content { get; set; }
     }
 }
---
+++ src/DbApplication/Models/ArticleDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbApplication.Models
+{
+    public class ArticleDbContext: DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql();
+        }
+    }
+}
