7176118a7b69e2ed1a034322ec26c6951200df0b
kwe <kwevans@gmail.com>
2016-07-11 00:03:03 +0100

    setup connection string

--- src/DbApplication/Models/ArticleDbContext.cs
+++ src/DbApplication/Models/ArticleDbContext.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DbApplication.Models
 {
     public class ArticleDbContext: DbContext
     {
+        private IConfiguration _config;
+
         public DbSet<Article> Articles { get; set; }
+
+        public ArticleDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
