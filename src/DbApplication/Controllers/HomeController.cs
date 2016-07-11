812431a7eaa764eb876e4bef707233ca2995c1dc
kwe <kwevans@gmail.com>
2016-07-11 02:26:48 +0100

    DB query now works!
    Woooohoooo!

--- src/DbApplication/Controllers/HomeController.cs
+++ src/DbApplication/Controllers/HomeController.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DbApplication.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace DbApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private IConfiguration _config;
+        private ArticleDbContext _context;
+        public HomeController(IConfiguration config, ArticleDbContext context)
+        {
+            _config = config;
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var Articles = _context.Articles.ToList();
+            return View(Articles);
         }
 
         public IActionResult About()
